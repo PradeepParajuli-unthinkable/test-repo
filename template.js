da5dc16fe17ab753e953f383f1abf6ccc3d23edd
PradeepParajuli-unthinkable <100185026+PradeepParajuli-unthinkable@users.noreply.github.com>
2025-04-28 11:59:39 +0530

    Update template.js
    Updated readmore button

--- template.js
+++ template.js
@@ -40,7 +40,7 @@
         static enableIngest = 0 == 1;
         static pageInitTime = Date.now();
 
-        static enableLatestNews = ('false' == 'true') ?? false;
+        static enableLatestNews = ('false' !== 'true') ?? false;
         static latestNewsParagraphId = parseInt('1') || 0;
         static latestNewsSpacementStyle = 'pixel' || 'pixel';
         static latestNewsSpacementValue = parseInt('50') || 0;
