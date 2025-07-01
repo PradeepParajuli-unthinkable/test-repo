68e2adb5e7d7fbcebd3dd56733732d61ad2a7e5f
PradeepParajuli-unthinkable <100185026+PradeepParajuli-unthinkable@users.noreply.github.com>
2025-07-01 18:18:42 +0530

    Update template.js
    redirect signalR to local server

--- template.js
+++ template.js
@@ -10,8 +10,8 @@
     // Signal R
     class SignalRMythDev {
         static isSignalREnabled = true;
-        //static signalRUrl = "http://localhost:5099/adhub";
-        static signalRUrl = "https://ingest.myth.ad/adhub";
+        static signalRUrl = "http://localhost:5099/adhub";
+        //static signalRUrl = "https://ingest.myth.ad/adhub";
         static signalRAutoReconnect = [0, 2000, 5000, 10000];
         static connection = null;
         static messageQueue = [];
